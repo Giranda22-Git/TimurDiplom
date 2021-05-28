71956204e31ca079866439f0c56933ec29c550c9
Giranda22-Git <aloxaeee@bk.ru>
2021-05-29 05:54:37 +0600

    fix

--- back/endPoints/news.js
+++ back/endPoints/news.js
@@ -7,49 +7,49 @@ const mongoNews = require('../models/news.js').mongoNews
 
 const tmpDir = __dirname + '/tmp/'
 const uploadDir = __dirname + '/tmp/'
-const upload = multer({dest: './endPoints/tmp/'})
+const upload = multer({ dest: './endPoints/tmp/' })
 
 // начало; получение всех новостей
 router.get('/', async (req, res) => {
-    const result = await mongoNews.find().exec()
-    res.status(200).send( JSON.stringify(result) )
+  const result = await mongoNews.find().exec()
+  res.status(200).send(JSON.stringify(result))
 })
 // конец; получение всех новостей
 
 
 // начало; получение картинки по id новости
 router.get('/load/:nid', async (req, res) => {
-    const resNews = await mongoNews.findById(req.params.nid)
-    res.sendFile(resNews.image)
+  const resNews = await mongoNews.findById(req.params.nid)
+  res.sendFile(resNews.image)
 })
 // конец; получение картинки по id новости
 
 
 // начало; создание новости
 router.post('/', upload.single('file'), async (req, res) => {
-    try {
-        const data = req.body
+  try {
+    const data = req.body
 
-        const image = req.file
+    const image = req.file
 
-        let imageString = null
+    let imageString = null
 
-        fs.renameSync(tmpDir + image.filename, uploadDir + 'image' + image.originalname)
-        imageString = uploadDir + 'image' + image.originalname
-        const newNews = new mongoNews({
-            title: data.title,
-            description: data.description,
-            image: imageString,
-        })
-        const result = await newNews.save()
-        const imagePath = 'http://localhost:3000/news/load/' + result._id
-        await mongoNews.updateOne({ title: data.title }, { imagePath: imagePath })
-        res.status(200).send( JSON.stringify( result ) )
-    }
-    catch(err) {
-        console.log(err)
-        res.sendStatus(500)
-    }
+    fs.renameSync(tmpDir + image.filename, uploadDir + 'image' + image.originalname)
+    imageString = uploadDir + 'image' + image.originalname
+    const newNews = new mongoNews({
+      title: data.title,
+      description: data.description,
+      image: imageString,
+    })
+    const result = await newNews.save()
+    const imagePath = 'http://localhost:3000/news/load/' + result._id
+    await mongoNews.updateOne({ title: data.title }, { imagePath: imagePath })
+    res.status(200).send(JSON.stringify(result))
+  }
+  catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
 })
 // конец; создание новости
 
